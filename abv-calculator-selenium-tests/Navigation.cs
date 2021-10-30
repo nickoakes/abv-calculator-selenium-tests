4920fa2bc5b937e3a5b795563d5522983ad69cb2
Nicholas Oakes <32389274+nickoakes@users.noreply.github.com>
2021-10-30 18:28:48 +0100

    First test added.

---
+++ abv-calculator-selenium-tests/Navigation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace abv_calculator_selenium_tests
+{
+    [TestFixture]
+    public class Navigation
+    {
+        IWebDriver _driver;
+        WebDriverWait _wait;
+
+        [SetUp]
+        public void Setup()
+        {
+            _driver = new ChromeDriver("C:/Users/Nick/source/repos/abv-calculator-selenium-tests/abv-calculator-selenium-tests");
+
+            _wait = new WebDriverWait(_driver, new TimeSpan(1000));
+
+            TestHelper.SetURL(_driver);
+        }
+
+        [Test]
+        public void Start_StartButtonClicked_DrinkSelectionStage()
+        {
+            TestHelper.CompleteStart(_driver, _wait);
+
+            bool result = TestHelper.GetVisibleElementInCollection(_driver.FindElements(By.TagName("h2"))).Text == "What have you made?";
+
+            Assert.IsTrue(result);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Close();
+        }
+    }
+}
---
+++ abv-calculator-selenium-tests/TestHelper.cs
@@ -0,0 +1,66 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.ObjectModel;
+
+namespace abv_calculator_selenium_tests
+{
+    public static class TestHelper
+    {
+        static string URL 
+        { 
+            get 
+            {
+                return "https://nickoakes.github.io/vue-abv-calculator/";
+            } 
+        }
+
+        /// <summary>
+        /// Sets the driver URL
+        /// </summary>
+        /// <param name="driver"></param>
+        public static void SetURL(IWebDriver driver)
+        {
+            driver.Url = URL;
+        }
+
+        /// <summary>
+        /// Returns the first element in a collection whose 'Displayed' property is true
+        /// </summary>
+        /// <param name="elements"></param>
+        /// <returns></returns>
+        public static IWebElement GetVisibleElementInCollection(ReadOnlyCollection<IWebElement> elements)
+        {
+            IWebElement element = null;
+
+            for(int i = 0; i < elements.Count; i++)
+            {
+                if (elements[i].Displayed)
+                {
+                    element = elements[i];
+
+                    break;
+                }
+            }
+
+            return element;
+        }
+
+        #region Navigation
+
+        /// <summary>
+        /// Clicks 'Start' to advance to the drink selection stage
+        /// </summary>
+        /// <param name="driver"></param>
+        /// <param name="wait"></param>
+        public static void CompleteStart(IWebDriver driver, WebDriverWait wait)
+        {
+            IWebElement startButton = driver.FindElement(By.CssSelector("button.v-btn"));
+
+            startButton.Click();
+
+            wait.Until(x => !startButton.Displayed);
+        }
+
+        #endregion
+    }
+}
