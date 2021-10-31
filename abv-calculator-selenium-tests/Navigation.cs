91dc01d54d02033641d7d9a8f799c50f04470493
Nicholas Oakes <32389274+nickoakes@users.noreply.github.com>
2021-10-31 13:50:48 +0000

    Basic navigation tests all in place.

--- abv-calculator-selenium-tests/Navigation.cs
+++ abv-calculator-selenium-tests/Navigation.cs
@@ -12,6 +12,8 @@ namespace abv_calculator_selenium_tests
         IWebDriver _driver;
         WebDriverWait _wait;
 
+        #region SetUp
+
         [SetUp]
         public void Setup()
         {
@@ -22,20 +24,146 @@ namespace abv_calculator_selenium_tests
             TestHelper.SetURL(_driver);
         }
 
+        #endregion
+
         [Test]
+        [Retry(3)]
         public void Start_StartButtonClicked_DrinkSelectionStage()
         {
-            TestHelper.CompleteStart(_driver, _wait);
+            try
+            {
+                TestHelper.NavigateToStage(_driver, _wait, (int)TestHelper.Stages.DrinkSelection);
 
-            bool result = TestHelper.GetVisibleElementInCollection(_driver.FindElements(By.TagName("h2"))).Text == "What have you made?";
+                bool result = TestHelper.CheckPromptText(_driver, "What have you made?");
 
-            Assert.IsTrue(result);
+                Assert.IsTrue(result);
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(false, ex.Message);
+            }
         }
+
+        [Test]
+        [Retry(3)]
+        public void DrinkSelectionStage_ContinueButtonClicked_BaseSugarStage()
+        {
+            try
+            {
+                TestHelper.NavigateToStage(_driver, _wait, (int)TestHelper.Stages.BaseSugar);
+
+                bool result = TestHelper.CheckPromptText(_driver, "The default sugar content for beer is 517.7 grams per one standard 40-pint beer kit");
+
+                Assert.IsTrue(result);
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(false, ex.Message);
+            }
+        }
+
+        [Test]
+        [Retry(3)]
+        public void BaseSugarStage_YesButtonClicked_AdditionalSugarStage()
+        {
+            try
+            {
+                TestHelper.NavigateToStage(_driver, _wait, (int)TestHelper.Stages.AdditionalSugar);
+
+                bool result = TestHelper.CheckPromptText(_driver, "Have you added any additional sugar?");
+
+                Assert.IsTrue(result);
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(false, ex.Message);
+            }
+        }
+
+        [Test]
+        [Retry(3)]
+        public void AdditionalSugarStage_NoButtonClicked_TotalVolumeStage()
+        {
+            try
+            {
+                TestHelper.NavigateToStage(_driver, _wait, (int)TestHelper.Stages.TotalVolume);
+
+                bool result = TestHelper.CheckPromptText(_driver, "What's the total volume?");
+
+                Assert.IsTrue(result);
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(false, ex.Message);
+            }
+        }
+
+        [Test]
+        [Retry(3)]
+        public void TotalVolumeStage_VolumeEnteredAndContinueClicked_ResultStage()
+        {
+            try
+            {
+                TestHelper.NavigateToStage(_driver, _wait, (int)TestHelper.Stages.Result);
+
+                bool result = TestHelper.CheckPromptText(_driver, "is approximately");
+
+                Assert.IsTrue(result);
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(false, ex.Message);
+            }
+        }
+
+        [Test]
+        [Retry(3)]
+        public void ResultStage_ResetClicked_StartStage()
+        {
+            try
+            {
+                TestHelper.NavigateToStage(_driver, _wait, (int)TestHelper.Stages.Result);
+
+                TestHelper.Reset(_driver, _wait);
+
+                bool result = TestHelper.CheckPromptText(_driver, "Use this calculator to work out the approximate A.B.V. of home-brewed drinks.");
+
+                Assert.IsTrue(result);
+            }
+            catch(Exception ex)
+            {
+                Assert.IsTrue(false, ex.Message);
+            }
+        }
+
+        [Test]
+        [Retry(3)]
+        public void TotalVolumeStage_BackButtonClickedRepeatedly_DrinkSelectionStage()
+        {
+            try
+            {
+                TestHelper.NavigateToStage(_driver, _wait, (int)TestHelper.Stages.TotalVolume);
+
+                while(!TestHelper.CheckPromptText(_driver, "What have you made?"))
+                {
+                    TestHelper.ClickButtonToAdvance(_driver, _wait, "back");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(false, ex.Message);
+            }
+        }
+
+        #region TearDown
 
         [TearDown]
         public void TearDown()
         {
             _driver.Close();
         }
+
+        #endregion
     }
 }
--- abv-calculator-selenium-tests/TestHelper.cs
+++ abv-calculator-selenium-tests/TestHelper.cs
@@ -6,12 +6,25 @@ namespace abv_calculator_selenium_tests
 {
     public static class TestHelper
     {
+
+        #region Generic
+
         static string URL 
         { 
             get 
             {
                 return "https://nickoakes.github.io/vue-abv-calculator/";
             } 
+        }
+
+        public enum Stages
+        {
+            Start,
+            DrinkSelection,
+            BaseSugar,
+            AdditionalSugar,
+            TotalVolume,
+            Result
         }
 
         /// <summary>
@@ -45,7 +58,60 @@ namespace abv_calculator_selenium_tests
             return element;
         }
 
+        /// <summary>
+        /// Returns a boolean value based on whether the prompt text displayed in the first visible <h2> element on the page contains the specified string
+        /// </summary>
+        /// <param name="driver"></param>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public static bool CheckPromptText(IWebDriver driver, string text)
+        {
+            return GetVisibleElementInCollection(driver.FindElements(By.TagName("h2"))).Text.Contains(text);
+        }
+
+        #endregion
+
         #region Navigation
+
+        /// <summary>
+        /// Clicks the currently displayed button with the specified text, and returns this button as an IWebElement
+        /// </summary>
+        /// <param name="driver"></param>
+        /// <param name="buttonText"></param>
+        /// <returns></returns>
+        public static IWebElement ClickButton(IWebDriver driver, string buttonText)
+        {
+            ReadOnlyCollection<IWebElement> buttons = driver.FindElements(By.ClassName("v-btn__content"));
+
+            IWebElement buttonClicked = null;
+
+            for(int i = 0; i < buttons.Count; i++)
+            {
+                if(buttons[i].Displayed && buttons[i].Text.ToLower().Contains(buttonText))
+                {
+                    buttons[i].Click();
+
+                    buttonClicked = buttons[i];
+
+                    break;
+                }
+            }
+
+            return buttonClicked;
+        }
+
+        /// <summary>
+        /// Clicks the currently displayed button with the specified text, and waits until this button is no longer visible
+        /// </summary>
+        /// <param name="driver"></param>
+        /// <param name="wait"></param>
+        /// <param name="buttonText"></param>
+        public static void ClickButtonToAdvance(IWebDriver driver, WebDriverWait wait, string buttonText)
+        {
+            IWebElement button = ClickButton(driver, buttonText);
+
+            wait.Until(x => !button.Displayed);
+        }
 
         /// <summary>
         /// Clicks 'Start' to advance to the drink selection stage
@@ -54,11 +120,112 @@ namespace abv_calculator_selenium_tests
         /// <param name="wait"></param>
         public static void CompleteStart(IWebDriver driver, WebDriverWait wait)
         {
-            IWebElement startButton = driver.FindElement(By.CssSelector("button.v-btn"));
+            ClickButtonToAdvance(driver, wait, "start");
+        }
 
-            startButton.Click();
+        /// <summary>
+        /// Selects the specified drink choice and clicks 'Continue' to advance to the base sugar stage
+        /// </summary>
+        /// <param name="driver"></param>
+        /// <param name="wait"></param>
+        /// <param name="drinkChoice"></param>
+        public static void CompleteDrinkSelection(IWebDriver driver, WebDriverWait wait, string drinkChoice)
+        {
+            ReadOnlyCollection<IWebElement> inputs = driver.FindElements(By.CssSelector(".v-input--selection-controls__input input[type='radio']"));
 
-            wait.Until(x => !startButton.Displayed);
+            for(int i = 0; i < inputs.Count; i++)
+            {
+                if(inputs[i].Displayed && inputs[i].GetAttribute("value") == drinkChoice)
+                {
+                    inputs[i].Click();
+
+                    break;
+                }
+            }
+
+            ClickButtonToAdvance(driver, wait, "continue");
+        }
+
+        /// <summary>
+        /// Clicks 'Yes' to set the base sugar mass to default, and advance to the additional sugar stage
+        /// </summary>
+        /// <param name="driver"></param>
+        /// <param name="wait"></param>
+        public static void CompleteBaseSugarWithDefault(IWebDriver driver, WebDriverWait wait)
+        {
+            ClickButtonToAdvance(driver, wait, "yes");
+        }
+
+        /// <summary>
+        /// Clicks 'No' to set the additional sugar mass to 0, and advance to the total volume stage
+        /// </summary>
+        /// <param name="driver"></param>
+        /// <param name="wait"></param>
+        public static void CompleteAdditionalSugarWithDefault(IWebDriver driver, WebDriverWait wait)
+        {
+            ClickButtonToAdvance(driver, wait, "no");
+        }
+
+        /// <summary>
+        /// Enters the total volume specified, and clicks 'Continue' to advance to the result stage
+        /// </summary>
+        /// <param name="driver"></param>
+        /// <param name="wait"></param>
+        /// <param name="totalVolume"></param>
+        public static void CompleteTotalVolume(IWebDriver driver, WebDriverWait wait, decimal totalVolume)
+        {
+            driver.FindElement(By.Id("input-74")).SendKeys(totalVolume.ToString());
+
+            ClickButtonToAdvance(driver, wait, "continue");
+        }
+
+        /// <summary>
+        /// Clicks the 'Reset' button to return to the start stage
+        /// </summary>
+        /// <param name="driver"></param>
+        /// <param name="wait"></param>
+        public static void Reset(IWebDriver driver, WebDriverWait wait)
+        {
+            ClickButtonToAdvance(driver, wait, "reset");
+        }
+
+        /// <summary>
+        /// Navigates to the stage specified by the shortest route possible
+        /// </summary>
+        /// <param name="driver"></param>
+        /// <param name="wait"></param>
+        /// <param name="stage"></param>
+        /// <param name="drinkChoice"></param>
+        /// <param name="totalVolume"></param>
+        public static void NavigateToStage(IWebDriver driver, WebDriverWait wait, int stage, string drinkChoice = "Beer", decimal totalVolume = 1)
+        {
+            int currentStage = 0;
+
+            while(currentStage < stage)
+            {
+                currentStage++;
+
+                switch (currentStage)
+                {
+                    case (int)Stages.DrinkSelection:
+                        CompleteStart(driver, wait);
+                        break;
+                    case (int)Stages.BaseSugar:
+                        CompleteDrinkSelection(driver, wait, drinkChoice);
+                        break;
+                    case (int)Stages.AdditionalSugar:
+                        CompleteBaseSugarWithDefault(driver, wait);
+                        break;
+                    case (int)Stages.TotalVolume:
+                        CompleteAdditionalSugarWithDefault(driver, wait);
+                        break;
+                    case (int)Stages.Result:
+                        CompleteTotalVolume(driver, wait, totalVolume);
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
 
         #endregion
